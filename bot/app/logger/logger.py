151c027ec4fdb16f23adda7d60a2cf83303e545a
IvanDrf <ivandrgmr@gmail.com>
2025-11-03 18:35:13 +0300

    dev: add logger setup

---
+++ bot/app/logger/logger.py
@@ -0,0 +1,25 @@
+import logging
+from sys import stdout
+from frozendict import frozendict
+
+from app.config.config import Config
+
+
+def configure_logger(cfg: Config) -> None:
+    logging.basicConfig(
+        level=select_logger_level(cfg.logger_level),
+        stream=stdout)
+
+
+type LoggerLevel = int | str
+
+levels: frozendict[str, LoggerLevel] = frozendict(
+    debug=logging.DEBUG,
+    info=logging.INFO,
+    warn=logging.WARN,
+    error=logging.ERROR
+)
+
+
+def select_logger_level(level: str) -> LoggerLevel:
+    return levels[level]
--- bot/main.py
+++ bot/main.py
@@ -1,11 +1,14 @@
 from asyncio import run
 
 from app.config.config import Config
+from app.logger.logger import configure_logger
 from app.app.app import App
 
 
 async def main():
     cfg: Config = Config.new(Config.get_config_path_from_flag())
+    configure_logger(cfg)
+
     app: App = await App.new(cfg)
 
     await app.run()
