65c356cb3cc5fcb44b67b5c5dc91185c93d4212e
IvanDrf <ivandrgmr@gmail.com>
2025-11-12 22:58:08 +0300

    dev: move work with message from commands -> handler (my)

--- bot/app/handlers/my/my.py
+++ bot/app/handlers/my/my.py
@@ -3,8 +3,11 @@ from aiogram.filters import Command
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 
+from typing import Optional
+
 from app.repo.repo import Repo
 from app.commands.my.my import MyLinker
+from app.models.link import Link
 
 user_links_router: Router = Router()
 
@@ -16,4 +19,19 @@ class UserLinksHandler:
         user_links_router.message(Command('my'))(self.print_user_links)
 
     async def print_user_links(self, message: Message, state: FSMContext) -> None:
-        await self.linker.find_user_links(message, state)
+        await state.clear()
+
+        if message.from_user is None:
+            await message.answer('Cant get your id, please try again')
+            return
+
+        links: Optional[tuple[Link, ...]] = await self.linker.find_user_links(message.from_user.id)
+        if links is None or len(links) == 0:
+            await message.answer('You dont have saved links, write /save to save some')
+            return
+
+        await message.answer(create_links_list(links))
+
+
+def create_links_list(links: tuple[Link, ...]) -> str:
+    return '\n'.join(f'{i+1}) {link.link}' for i, link in enumerate(links))
