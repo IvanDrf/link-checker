6806b786bab6e003c587b4949f16a216741d86e9
IvanDrf <ivandrgmr@gmail.com>
2025-11-12 22:50:19 +0300

    dev: move work with message from commands -> handler (save)

--- bot/app/commands/save/save.py
+++ bot/app/commands/save/save.py
@@ -1,9 +1,6 @@
-from aiogram.types import Message
-from aiogram.fsm.context import FSMContext
 from typing import Optional
 
 from app.repo.repo import Repo
-from app.commands.save.state import SaveState
 from app.models.user import User, MAX_LINKS_AMOUNT
 
 
@@ -11,34 +8,16 @@ class Saver:
     def __init__(self, repo: Repo) -> None:
         self.repo: Repo = repo
 
-    async def input_link(self, message: Message, state: FSMContext) -> None:
-        await state.clear()
-
-        await message.answer('Enter the link, you want to save')
-        await state.set_state(SaveState.waiting_input_link)
-
-    async def save_link(self, message: Message, state: FSMContext) -> None:
-        if message.text is None or message.from_user is None:
-            await message.answer('Cant get your message, try again')
-            return
-
-        user_id: int = message.from_user.id
-
+    async def save_link(self, user_id: int, link: str) -> str:
         user: Optional[User] = await self.repo.find_user(user_id)
         if user is None:
-            await message.answer('You are not in database, please write /start')
-            return
+            return 'You are not in database, please write /start'
 
         if user.links_amount >= MAX_LINKS_AMOUNT:
-            await message.answer(f'You have maximum saved links - {MAX_LINKS_AMOUNT}')
-            return
-
-        link: str = message.text
+            return f'You have maximum saved links - {MAX_LINKS_AMOUNT}'
 
         res: Optional[int] = await self.repo.save_link(link, user_id)
         if res is None:
-            await message.answer('Cant save your link, please try again')
-            return
+            return 'Cant save your link, please try again'
 
-        await message.answer(f'Successfully saved your link: {link}')
-        await state.clear()
+        return f'Successfully saved your link: {link}'
