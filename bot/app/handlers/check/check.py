b26caf44c2df847d68cd896d22d37e643657c0d0
IvanDrf <ivandrgmr@gmail.com>
2025-11-18 13:52:20 +0300

    feat: add measuring time for checking links

--- bot/app/handlers/check/check.py
+++ bot/app/handlers/check/check.py
@@ -4,10 +4,8 @@ from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 
 from typing import Final
+from time import time
 
-from app.config.config import Config
-from app.repo.abstraction import IRepo
-from app.repo.redis import RedisRepo
 from app.commands.check.check import Checker
 from app.exc.internal import InternalError
 from app.exc.external import ExternalError
@@ -33,11 +31,15 @@ class CheckHandler:
             await message.answer('Cant get your id, please try again', reply_markup=ReplyKeyboardRemove())
             return
 
+        await message.answer('Starting to check links...')
+
         try:
+            start_time: float = time()
             message_answer = await self.checker.check_links(message.from_user.id, message.chat.id)
 
             await message.answer(message_answer, reply_markup=ReplyKeyboardMarkup(
                 keyboard=CheckHandler.BUTTONS, resize_keyboard=True))
+            await message.answer(f'Time: {(time() - start_time):.3f} sec')
 
         except UserError as e:
             message_answer: str = e.__str__()[e.__str__().find(':') + 1:]
